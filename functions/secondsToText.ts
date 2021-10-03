f952754379b984d3f80487cf32e10d44eee1231b
RileyGe <grlsr@163.com>
2021-10-03 12:41:53 +0800

    更新翻译

--- functions/secondsToText.ts
+++ functions/secondsToText.ts
@@ -1,10 +1,15 @@
+import { t } from '@lingui/macro'
+import { useLingui } from '@lingui/react'
+
 export function secondsRender(d): string {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const { i18n } = useLingui()
     d = Number(d)
     let h = Math.floor(d / 3600)
     let m = Math.floor((d % 3600) / 60)
     let s = Math.floor((d % 3600) % 60)
-    let hDisplay = h > 0 ? h + (h === 1 ? ' hour, ' : ' hours, ') : ''
-    let mDisplay = m > 0 ? m + (m === 1 ? ' minute, ' : ' minutes, ') : ''
-    let sDisplay = s > 0 ? s + (s === 1 ? ' second' : ' seconds') : ''
+    let hDisplay = h > 0 ? h + (h === 1 ? ' ' + i18n._(t`hour`) + ', ' : ' ' + i18n._(t`hours`) + ', ') : ''
+    let mDisplay = m > 0 ? m + (m === 1 ? ' ' + i18n._(t`minute`) + ', ' : ' ' + i18n._(t`minutes`) + ', ') : ''
+    let sDisplay = s > 0 ? s + (s === 1 ? ' ' + i18n._(t`second`) : ' ' + i18n._(t`seconds`)) : ''
     return hDisplay + mDisplay + sDisplay
 }
